f01dcad80adb3e79111b27bcf6ddd89e4cfdda10
andrewoc1 <andrewoc1111@gmail.com>
2026-07-23 22:56:08 -0400

    Complete implementation and dynamic screen loading

--- src/index.js
+++ src/index.js
@@ -4,4 +4,19 @@ import loadMenu from "./menu.js";
 import "./style.css";
 
 const container = document.querySelector("#container");
-container.append(loadAbout());
+
+const homeBtn = document.querySelector("#home-btn");
+const menuBtn = document.querySelector("#menu-btn");
+const aboutBtn = document.querySelector("#about-btn");
+
+homeBtn.addEventListener("click", () => {
+  container.replaceChildren(loadHome());
+});
+
+menuBtn.addEventListener("click", () => {
+  container.replaceChildren(loadMenu());
+});
+
+aboutBtn.addEventListener("click", () => {
+  container.replaceChildren(loadAbout());
+});
